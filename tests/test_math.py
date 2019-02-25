0c5ce7d74f1cbaabbf298fc6ec4c9ea4c085a1b7
Pål Grønås Drange <PGDR@statoil.com>
2019-02-25 21:45:14 +0100

    Add test for future feature

--- tests/test_math.py
+++ tests/test_math.py
@@ -91,6 +91,69 @@ def fragment():
         gl_FragColor = vec4(color_gamma, 1.0)
 
 
+
+
+
+TRANSFORMERS = """
+uniform vec3 light_position;
+
+attribute vec3 position;
+attribute vec3 color;
+attribute float radius;
+
+varying float v_size;
+varying vec3 v_color;
+varying float v_radius;
+varying vec4 v_eye_position;
+varying vec3 v_light_direction;
+
+void main (void)
+{
+    v_color = color;
+    v_radius = radius;
+    v_eye_position = <transform.trackball_view> *
+                     <transform.trackball_model> *
+                     vec4(position,1.0);
+    v_light_direction = normalize(light_position);
+    gl_Position = <transform(position)>;
+    // stackoverflow.com/questions/8608844/...
+    //  ... resizing-point-sprites-based-on-distance-from-the-camera
+    vec4 p = <transform.trackball_projection> *
+             vec4(radius, radius, v_eye_position.z, v_eye_position.w);
+    v_size = 512.0 * p.x / p.w;
+    gl_PointSize = v_size + 5.0;
+}
+"""
+def transformers():
+    light_position : uniform[vec3]
+    position : attribute[vec3]
+    color : attribute[vec3]
+    radius : attribute[float]
+
+    v_size : varying[float]
+    v_color : varying[vec3]
+    v_radius : varying[float]
+    v_eye_position : varying[vec4]
+    v_light_direction : varying[vec3]
+
+    def main(void) -> void:
+        v_color = color
+        v_radius = radius
+        v_eye_position = 0@transform.trackball_view  * 0@transform.trackball_model * vec4(position, 1.0)
+        v_light_direction = normalize(light_position)
+        gl_Position = 0@transform(position)
+
+        p : vec4 = 0@transform.trackball_projection * vec4(radius, radius, v_eye_position.z, v_eye_position.w)
+        v_size = 512.0 * p.x / p.w
+        gl_PointSize = v_size + 5.0
+
+
+
+
+
+
+
+
 class MathTest(unittest.TestCase):
 
     def eqcode(self, act, exp):
@@ -101,6 +164,11 @@ class MathTest(unittest.TestCase):
 
     def test_fragment(self):
         self.eqcode(FRAGMENT, cdecorator.transpile(fragment))
+
+    def test_transformers(self):
+        # TODO
+        # self.eqcode(TRANSFORMERS, cdecorator.transpile(transformers))
+        pass
 
 
 if __name__ == '__main__':
